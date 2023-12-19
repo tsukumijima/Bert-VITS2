cce2bc9f8790750f44542f86b3c31010bb8a23ae
tsukumi <tsukumijima@users.noreply.github.com>
2023-12-19 15:46:41 +0000

    Update: Gradio WebUI が 0.0.0.0 でリッスンされるようにする 私のように Ubuntu のサーバー PC で実行していて WebUI を他デバイスから操作したいケースで必要

--- webui_preprocess.py
+++ webui_preprocess.py
@@ -163,4 +163,4 @@ if __name__ == "__main__":
         bert_gen_btn.click(bert_gen, inputs=[data_dir], outputs=[info])
 
     webbrowser.open("http://127.0.0.1:7860")
-    app.launch(share=False, server_port=7860)
+    app.launch(share=False, server_name='0.0.0.0', server_port=7860)
